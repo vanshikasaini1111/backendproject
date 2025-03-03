60443da1cab11c314cf32e5b80bda98e50b03622
Vanshika Saini <vanshikasaini736@gmail.com>
2025-03-03 23:36:21 +0530

    user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,7 +4,15 @@ import {registerUser} from "../controllers/user.controllers.js"
 import {upload} from "../middlewares/multer.middlewares.js"
 import {loginUser} from "../controllers/user.controllers.js"
 import {logOutUser} from "../controllers/user.controllers.js"
-import {refreshAccessToken}from "../controllers/user.controllers.js"
+import {refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory
+}from "../controllers/user.controllers.js"
 import {verifyJWT} from "../middlewares/authentication.middlewares.js"
 const router=Router();
 console.log("userroutes running");
@@ -26,6 +34,21 @@ router.route("/register").post(
     // using middleware
     router.route("/logout").post(verifyJWT,logOutUser);
     router.route("/refresh-token").post(refreshAccessToken);
+    router.route("/change-password").post(verifyJWT,
+        changeCurrentPassword);
+    router.route("/current-user").get(verifyJWT,
+        getCurrentUser);
+    router.route("/update-account").patch(verifyJWT,
+            updateAccountDetails);
+    router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),
+             updateUserAvatar);
+    router.route("/cover-image").path(verifyJWT,upload.single("coverImage"),
+                updateUserCoverImage);
+    router.route("/c/:username").get(verifyJWT,
+       getUserChannelProfile );  
+    router.route("/watchhistory").get(verifyJWT,
+    getWatchHistory );            
+        
 export default router;
 
 
