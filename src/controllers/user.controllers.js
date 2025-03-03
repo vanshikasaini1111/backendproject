48dac5ad6f4aab8bfacccb46fe553257204d88af
Vanshika Saini <vanshikasaini736@gmail.com>
2025-03-03 23:24:46 +0530

    watch history

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -374,6 +374,52 @@ const getUserChannelProfile=asyncHandler(async(req,res)=>{
   .json(new apiResponse(200,channel[0],"user channel fetched successfully"))
 })
 
+const getWatchHistory=asyncHandler(async(req,res)=>{
+const user=await User.aggregate([
+  {
+    $match:{
+      _id:new mongoose.Types.ObjectId(req.user._id)
+      //we get a string which mongoose converts into mongodb id
+
+    }
+  },{
+    $lookup:{
+       from:"videos",
+       localField:"watchHistory",
+       foreignField:"_id",
+       as:"watchHistory",
+       pipeline:[{
+        
+        $lookup:{
+             from:"users",
+             localField:"owner",
+             foreignField:"_id",
+             as:"owner",
+             pipeline:[{
+              $project:{
+                fullName:1,
+                userName:1,
+                avatar:1,
+              }
+             }]
+        }
+      
+       },
+      {
+        $addFields:{
+          owner:{
+            $first:"$owner"
+          }
+        }
+      }]
+    }
+  }
+])
+
+return res.status(200).json(new apiResponse(200,user[0].watchHistory,
+  "watch history fetched successfully"))
+})
+
 export {
   registerUser,
   loginUser,
@@ -384,6 +430,7 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
-  getUserChannelProfile
+  getUserChannelProfile,
+  getWatchHistory
 };
 
