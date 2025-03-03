b467ed17d3001b1699d5f416e9ad61090c0bd185
Vanshika Saini <vanshikasaini736@gmail.com>
2025-03-03 23:37:27 +0530

    lil change

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -42,7 +42,7 @@ router.route("/register").post(
             updateAccountDetails);
     router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),
              updateUserAvatar);
-    router.route("/cover-image").path(verifyJWT,upload.single("coverImage"),
+    router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),
                 updateUserCoverImage);
     router.route("/c/:username").get(verifyJWT,
        getUserChannelProfile );  
